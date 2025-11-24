9c671bbe755793da3f20ef8594358119ec400cf1
marcopra <marco.prattico@live.it>
2025-11-24 16:34:14 +0000

    sac scripts

---
+++ sac_rooms_2.sh
@@ -0,0 +1,12 @@
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_2 seed=0
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_2 seed=1
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_2 seed=2
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_2 seed=3
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_2 seed=4
+
+
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_2 seed=0
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_2 seed=1
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_2 seed=2
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_2 seed=3
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_2 seed=4
