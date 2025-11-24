96991900d4636026641bbeb51487e48eeebbbf06
marcopra <marco.prattico@live.it>
2025-11-24 16:38:41 +0000

    sac scripts

--- sac_rooms_1.sh
+++ sac_rooms_1.sh
@@ -1,12 +1,12 @@
-python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 seed=0
-python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 seed=1
-python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 seed=2
-python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 seed=3
-python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 seed=4
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 device=cuda:1 seed=0
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 device=cuda:1 seed=1
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 device=cuda:1 seed=2
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 device=cuda:1 seed=3
+python train.py use_wandb=true eval_every_frames=2000 p_path="/home/mprattico/distribution_matching/models/four_rooms5/policy_operator.npy" configs/env=four_rooms5_1 device=cuda:1 seed=4
 
 
-python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 seed=0
-python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 seed=1
-python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 seed=2
-python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 seed=3
-python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 seed=4
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 device=cuda:1 seed=0
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 device=cuda:1 seed=1
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 device=cuda:1 seed=2
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 device=cuda:1 seed=3
+python train.py use_wandb=true eval_every_frames=2000 configs/env=four_rooms5_1 device=cuda:1 seed=4
