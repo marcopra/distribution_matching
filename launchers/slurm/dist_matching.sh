7156653fc3034d7e8473c3100f63e8eed833ff70
marcopra <marco.prattico@live.it>
2025-11-26 12:17:36 +0100

    slurm launchre

--- launchers/slurm/dist_matching.sh
+++ launchers/slurm/dist_matching.sh
@@ -2,7 +2,6 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:1
 #SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 #SBATCH --error=%j.err
@@ -19,4 +18,4 @@ source ~/.bashrc
 conda activate dist_matching
 
 # Use quotes and escape model path appropriately
-python3 train_metaworld.py agent.pretrained_path=\"${MODEL_PATH}\" exp_name=\"${EXP_NAME}\" seed=$SEED_ARG env_name=$ENV_NAME random_init=$RANDOM_HAND random_goal=$RANDOM_GOAL wandb_tag=$WANDB_TAG wandb_project=$WANDB_PROJECT num_train_frames=220000 agent.freeze_encoder=$FREEZE agent.no_taco=$NO_TACO agent=$AGENT
+python distribution_matching.py env=four_rooms10_0 experimemt.print_every=1000
