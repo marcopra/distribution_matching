1b1a00f1f7ad39715b3f424477c5e05b93a363d5
marcopra <marco.prattico@live.it>
2026-01-20 20:20:19 +0000

    launchers

--- launchers/slurm/smm_discrete/smm_launcher.sh
+++ launchers/slurm/smm_discrete/smm_launcher.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 seeds="0 1 2"
-envs="multiplerooms10_3x3 four_rooms10_2 two_rooms15_0"
-
+envs=("multiplerooms10_3x3" 
+"four_rooms10_2" 
+"two_rooms15_0")
 
 
 for seed in $seeds; do   
