58a504816bb764c19d46a1773cf0ac501ac1e700
marcopra <marco.prattico@live.it>
2026-01-20 16:10:45 +0000

    new launchers

---
+++ launchers/slurm/cic_discrete/cic_launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+seeds="0 1 2"
+envs="multiplerooms10_3x3 four_rooms10_2 two_rooms15_0"
+
+
+
+for seed in $seeds; do   
+    for env in "${envs[@]}"; do
+        sbatch --export=SEED="${seed}",ENV="${env}" launchers/slurm/cic_discrete/cic_base.sh
+            
+    done
+done
