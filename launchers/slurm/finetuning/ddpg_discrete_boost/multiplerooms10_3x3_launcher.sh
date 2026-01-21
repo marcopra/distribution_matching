f595a828497f2f49893545583eeae901af334eee
marcopra <marco.prattico@live.it>
2026-01-21 13:58:08 +0000

    mdifyied path

--- launchers/slurm/finetuning/ddpg_discrete_boost/multiplerooms10_3x3_launcher.sh
+++ launchers/slurm/finetuning/ddpg_discrete_boost/multiplerooms10_3x3_launcher.sh
@@ -5,8 +5,8 @@ envs=("multiplerooms10_3x3_0"
        "multiplerooms10_3x3_1" 
        "multiplerooms10_3x3_2" )
 model_path=(
-    "/home/mprattico/distribution_matching/models/rnd/multiplerooms10_3x3/snapshot.pt"
-    "none")
+  "/home/mprattico/distribution_matching/models/boost/multiplerooms10_3x3/snapshot.pt"
+  )
 
 
 for seed in $seeds; do   
