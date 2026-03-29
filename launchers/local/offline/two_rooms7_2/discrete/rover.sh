a78ef27dcb8c400858cd7df45e64e8e3b8afe5ab
marcopra <marco.prattico@live.it>
2026-03-29 12:54:58 +0000

    launchers

--- launchers/local/offline/two_rooms7_2/discrete/rover.sh
+++ launchers/local/offline/two_rooms7_2/discrete/rover.sh
@@ -2,10 +2,10 @@
 
 CUDA_DEVICE=${1:-0}
 
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=0 # device=cuda:${CUDA_DEVICE}
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=1 # device=cuda:${CUDA_DEVICE}
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=2 # device=cuda:${CUDA_DEVICE}
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=3 # device=cuda:${CUDA_DEVICE}
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=4 # device=cuda:${CUDA_DEVICE}
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=5 # device=cuda:${CUDA_DEVICE}
-python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico-iit.local/distribution_matching/models/discrete/two_rooms/rover seed=6 # device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=0 device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=1 device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=2 device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=3 device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=4 device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=5 device=cuda:${CUDA_DEVICE}
+python train_offline.py --config-name=train/train_offline_rooms env=two_rooms7_2 wandb_tag=rover replay_buffer_dir=/home/mprattico/distribution_matching/models/discrete/two_rooms/rover seed=6 device=cuda:${CUDA_DEVICE}
