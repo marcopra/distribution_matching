0d0813b9c344ca472adaadfc6476b20f755f50e1
marcopra <marco.prattico@live.it>
2026-04-19 20:12:04 +0200

    pointmaze launcher

---
+++ launchers/slurm/fetch/pretraining/rover/rover_launcher.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+seeds="1"
+envs=(
+  pointmaze/pointmaze_umaze_goal_1
+)
+lr_actors=(
+    200
+)
+# Indices into the sink_schedules array defined in rover_hparam_base.sh:
+#   0 -> linear(0.0, 0.0001, 1_000_000)
+#   1 -> linear(0.0, 0.001,  2_000_000)
+#   2 -> linear(0.0, 1,      1_000_000)
+#   3 -> linear(0.0, 0.0001, 500_000)
+#   4 -> linear(1.0, 1.0,    100_000)
+
+sink_idxs=(0 1 2 3 4) 
+batch_sizes_actor=(
+    5000
+)
+feature_dims=(
+    100
+    256
+    512
+)
+
+for seed in $seeds; do
+    for env in "${envs[@]}"; do
+        for lr_actor in "${lr_actors[@]}"; do
+            for sink_idx in "${sink_idxs[@]}"; do
+                for batch_size_actor in "${batch_sizes_actor[@]}"; do
+                    for feature_dim in "${feature_dims[@]}"; do
+                        sbatch --export=SEED="${seed}",ENV="${env}",LR_ACTOR="${lr_actor}",SINK_IDX="${sink_idx}",BATCH_SIZE_ACTOR="${batch_size_actor}",FEATURE_DIM="${feature_dim}" launchers/slurm/fetch/pretraining/rover/rover_base.sh
+                    done
+                done
+            done
+        done
+    done
+done
