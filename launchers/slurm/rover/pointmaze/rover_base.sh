125cc3e1ada5ccb3255841d6959ea03b4c728e18
marcopra <marco.prattico@live.it>
2026-04-27 12:08:22 +0200

    configs and launchers

--- launchers/slurm/rover/pointmaze/rover_base.sh
+++ launchers/slurm/rover/pointmaze/rover_base.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 #SBATCH --error=%j.err
-#SBATCH --partition=gpua
+#SBATCH --partition=gpuv
 
 cd $SLURM_SUBMIT_DIR
 
@@ -17,11 +17,6 @@ conda activate dist_matching
 
 export HYDRA_FULL_ERROR=1
 
-# if subsamples is none, we set the SUBSAMPLES equal to null
-if [ "$SUBSAMPLES" = "none" ]; then
-    SUBSAMPLES=null
-    EXTRA_ARGS="agent.lambda_reg=1e-6"
-fi
 
 # Decode sink_schedule from index to avoid quoting issues in sbatch --export
 sink_schedules=(
@@ -37,9 +32,9 @@ SINK_SCHEDULE="${sink_schedules[$SINK_IDX]}"
 
 python pretrain.py \
     --config-name=pretrain/pretrain_pointmaze_umaze_1 \
-    agent=rover_nystrom_memory_efficient \
+    agent=rover \
     use_wandb=true \
-    agent.subsamples=${SUBSAMPLES} \
+    agent.batch_size_actor=${BATCH_SIZE_ACTOR} \
     agent.lr_actor=${LR_ACTOR} \
     agent.feature_dim=${FEATURE_DIM}\
     seed=${SEED} \
@@ -48,5 +43,5 @@ python pretrain.py \
     agent.update_every_steps=100 \
     agent.pmd_steps=100 \
     num_train_frames=200_000 \
-    ${EXTRA_ARGS} 
+
 
--- launchers/slurm/rover/pointmaze/rover_launcher.sh
+++ launchers/slurm/rover/pointmaze/rover_launcher.sh
@@ -6,21 +6,21 @@ envs=(
 )
 
 
-subsamples=(
+batch_size_actors=(
 # "none"
-# "8000"
-"4000"
-"2000"
+"8200"
+"4100"
+# "2000"
 # "500"
 # "100"
 # "20"
 )
 
 feature_dims=(
-    64
+    # 64
     128
-    #256
-    #512
+    256
+    # 512
 )
 
 lr_actors=( 
@@ -39,11 +39,11 @@ sink_idxs=(0 1)
 
 for seed in $seeds; do   
     for env in "${envs[@]}"; do
-        for subsamples in "${subsamples[@]}"; do
+        for bs in "${batch_size_actors[@]}"; do
             for lr_actor in "${lr_actors[@]}"; do
                 for sink_idx in "${sink_idxs[@]}"; do
                     for feature in "${feature_dims[@]}"; do
-                        sbatch --export=SEED="${seed}",ENV="${env}",SUBSAMPLES="${subsamples}",LR_ACTOR="${lr_actor}",SINK_IDX="${sink_idx}",FEATURE_DIM="${feature}" launchers/slurm/nystrom/rover/pointmaze/rover_memory_efficient_base.sh
+                        sbatch --export=SEED="${seed}",ENV="${env}",BATCH_SIZE_ACTOR="${bs}",LR_ACTOR="${lr_actor}",SINK_IDX="${sink_idx}",FEATURE_DIM="${feature}" launchers/slurm/rover/pointmaze/rover_base.sh
                     done
                 done
             done
