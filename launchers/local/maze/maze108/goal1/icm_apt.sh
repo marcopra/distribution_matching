b267869bed73b5f5eb75628f21e167fa346a6aae
marcopra <marco.prattico@live.it>
2026-05-03 15:00:55 +0200

    launchers

---
+++ launchers/local/maze/maze108/goal1/icm_apt.sh
@@ -0,0 +1,9 @@
+CUDA_DEVICE=${1:-0}
+
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=0 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=1 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=2 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=3 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=4 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=5 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
+python train.py --config-name=train/finetune_maze agent=icm_apt_discrete p_path="/home/mprattico/distribution_matching/models/maze/108/pixels/icm_apt/snapshot.pt" seed=6 env=gridworld/maze_108_seed7_env agent.actor_lr=1e-4 obs_type=pixels device=cuda:${CUDA_DEVICE}; 
---
+++ launchers/local/maze/maze108/goal1/scratch.sh
@@ -0,0 +1,9 @@
+CUDA_DEVICE=${1:-0}
+
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=0 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=1 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=2 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=3 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=4 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=5 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
+python train.py --config-name=train/finetune_maze agent=ddpg_discrete seed=6 env=gridworld/maze_108_seed7_env obs_type=pixels device=cuda:${CUDA_DEVICE} agent.actor_lr=1e-4; 
