91e1953779fbced23be8d4e8c6b22d241f6f8e29
marcopra <marco.prattico@live.it>
2026-07-11 11:36:15 +0200

    minor

--- launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_launcher.sh
+++ launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_launcher.sh
@@ -17,7 +17,7 @@ subsamples=(
 for seed in $seeds; do
     for batch_size_actor in "${batch_sizes_actor[@]}"; do
         for subsample in "${subsamples[@]}"; do
-            sbatch --export=SEED=$seed,BATCH_SIZE_ACTOR=$batch_size_actor,SUBSAMPLE=$subsample launchers/slurm/pretraining/atari/montezouma/rover_nystrom_base.sh
+            sbatch --export=SEED=$seed,BATCH_SIZE_ACTOR=$batch_size_actor,SUBSAMPLE=$subsample launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_base.sh
         done
     done
 done
