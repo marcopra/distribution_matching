24941ecc36f2f6fecc421d4a15b1a998bdb0914d
marcopra <marco.prattico@live.it>
2026-07-17 10:50:30 +0200

    montezouma cholesky

--- launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_base.sh
+++ launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_base.sh
@@ -4,10 +4,10 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=96G
 #SBATCH --gres=gpu:1
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=%j.out
 #SBATCH --error=%j.err
-#SBATCH --partition=gpua-longrun
+#SBATCH --partition=gpua
 
 cd $SLURM_SUBMIT_DIR
 
@@ -17,15 +17,7 @@ conda activate dist_matching
 
 export HYDRA_FULL_ERROR=1
 
-# Decode sink_schedule from index to avoid quoting issues in sbatch --export
-sink_schedules=(
-    "linear(0.0, 0.0001, 1_000_000)"
-    "linear(0.0, 0.001,  2_000_000)"
-    "linear(0.0, 1,      1_000_000)"
-    "linear(0.0, 0.0001, 500_000)"
-    "linear(1.0, 1.0,    100_000)"
-)
-SINK_SCHEDULE="${sink_schedules[$SINK_IDX]}"
+
 
 
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python pretrain_parallel.py \
@@ -33,8 +25,10 @@ PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True python pretrain_parallel.py \
     wandb_tag=parallel \
     use_wandb=true \
     seed=${SEED} \
+    agent.feature_dim=${FEATURE_DIM} \
     agent.batch_size_actor=${BATCH_SIZE_ACTOR} \
     agent.subsamples=${SUBSAMPLE} \
     agent.sink_schedule=0.0 \
-    agent.encoded_fifo_capacity=${BATCH_SIZE_ACTOR} 
+    agent.encoded_fifo_capacity=${BATCH_SIZE_ACTOR} \
+    wandb_project=montezouma_hp
 
--- launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_launcher.sh
+++ launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_launcher.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-seeds="1 2 3"
+seeds="1"
 
 batch_sizes_actor=(
-    # 50000
+    50000
     100000
     # 200000
 )
 
 subsamples=(
-    # 5000
+    5000
     10000
     # 20000
 )
 
+feature_dims=(
+    64
+    256
+    # 512
+    # 1024
+)\
 for seed in $seeds; do
     for batch_size_actor in "${batch_sizes_actor[@]}"; do
         for subsample in "${subsamples[@]}"; do
-            sbatch --export=SEED=$seed,BATCH_SIZE_ACTOR=$batch_size_actor,SUBSAMPLE=$subsample launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_base.sh
+            for feature_dim in "${feature_dims[@]}"; do
+                sbatch --export=SEED=$seed,BATCH_SIZE_ACTOR=$batch_size_actor,SUBSAMPLE=$subsample,FEATURE_DIM=$feature_dim launchers/slurm/pretraining/atari/montezouma/parallel_rover_nystrom_base.sh
+            done
         done
     done
 done
