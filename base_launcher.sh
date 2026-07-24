37ef6ce245b0da5c2b18aae39ff2333c5de82029
marcopra <marco.prattico@live.it>
2026-07-24 16:41:43 +0200

    new laucnher

--- base_launcher.sh
+++ base_launcher.sh
@@ -2,19 +2,23 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
+#SBATCH --mem=96G
 #SBATCH --gres=gpu:1
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=%j.out
 #SBATCH --error=%j.err
-#SBATCH --partition=gpua
+#SBATCH --partition=gpua-longrun
 
 cd $SLURM_SUBMIT_DIR
 
 # Load environment
 source ~/.bashrc
-conda activate cleanrl311
-
+conda activate dist_matching
 
 export HYDRA_FULL_ERROR=1
 
-python cleanrl_rnd_ppo.py --track --wandb_project_name "montezouma_nystrom" --seed 2
+
+
+
+python pretrain_parallel.py --config-name=pretrain_parallel/pretrain_montezouma_random agent=random wandb_tag=parallel_random use_wandb=true seed=1 wandb_project=montezouma_unique
+
